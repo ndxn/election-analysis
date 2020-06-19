673790f1b61b915059cb030bb344c777775b781f
Neal Dixon <Neal@Neals-MacBook-Pro.local>
2020-06-19 06:22:37 -0700

    Periodic push

--- PyPoll.py
+++ PyPoll.py
@@ -5,9 +5,17 @@
 # 4. The percentage of votes each candidate won
 # 5. The winner of the election based on popular vote
 
+# Import CSV
+import csv
+
 # Import the datetime class from the datetime module.
 import datetime as dt
+
+
 # Use the now() attribute on the datetime class to get the present time.
 now = dt.datetime.now()
 # Print the present time.
 print("The time right now is ", now)
+
+# Open the election results and read the file.
+electionResults = open(Resources/election_results.csv,r)
