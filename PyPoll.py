0ef74b8fb5ad2d85260fdaea19c583fa391d0b25
Neal Dixon <Neal@Neals-MacBook-Pro.local>
2020-06-18 17:02:51 -0700

    Adding new files

---
+++ PyPoll.py
@@ -0,0 +1,13 @@
+# Data we need to retrieve
+# 1. Total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The total number of votes each candidate won
+# 4. The percentage of votes each candidate won
+# 5. The winner of the election based on popular vote
+
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
