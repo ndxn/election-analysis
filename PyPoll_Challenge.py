8870fb9a4f3e9ffadebc665d34ecbc1611c8516d
Neal Dixon <Neal@Neals-MacBook-Pro.local>
2020-06-20 21:44:45 -0700

    Adding analysis folder

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -1,22 +1,10 @@
-# NOTES FOR MODULE 3 CHALLENGE
-# The challenge ask for additional functionality to be added to the code in order to tally votes by county. Because the
-# code already iterated through the csv file, I opted to add the funcationality to the existing for loop and if statement
-# which should reduce the amount of time it takes for the program to run. Directly below in the notes are the instructions
-# for the challenge. Comments are added therein to explain where the work has been added. 
-
-# 1. Create a list for the counties.
-    # See Line 
-# 2. Create a dictionary where the county is the key and the votes cast for each county in the election are the values.
-# 3. Create an empty string that will hold the county name that had the largest turnout.
-# 4. Declare a variable that represents the number of votes that a county received.
-
 # Add our dependencies.
 import csv
 import os
-# Assign a variable to load a file from a path.
-file_to_load = os.path.join('/Users/Neal/DataAustin2020/Challenges/election-analysis/Resources/election_results.csv')
+# Assign a variable to load a file from a path. 
+file_to_load = os.path.join("Resources/election_results.csv")
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("/Users/Neal/DataAustin2020/Challenges/election-analysis/Analysis/election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Initialize a total vote counter.
 total_votes = 0
 # Candidate options and candidate votes.
@@ -77,7 +65,7 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # MOD 3 CHALLENGE - Print out voter turnout by county
-    counties_section_header = "\nCounties:"
+    counties_section_header = "\nCounties:\n"
     print(counties_section_header)
     txt_file.write(counties_section_header)
     for county in county_votes:
@@ -85,20 +73,21 @@ with open(file_to_save, "w") as txt_file:
         votes = county_votes[county]
         county_percentage = float(votes) / float(total_votes) * 100
         county_results = (
-            f"{county}: {county_percentage:.1f}% ({votes:,})")
+            f"{county}: {county_percentage:.1f}% ({votes:,})\n")
         # Print vote count by county and percentage to the terminal.
         print(county_results)
         #  Save the candidate results to our text file.
         txt_file.write(county_results)
         # Determine county vote count, county representation, and county with most votes.
         if (votes > county_highest_count):
+            county_highest_count = votes
             county_with_highest_votes = county
 
     # Print the largest county turnout results to the terminal.
     county_highest_turnout_summary = (
         f"\n-------------------------\n"
         f"Largest County Turnout: {county_with_highest_votes}\n"
-        f"-------------------------")
+        f"-------------------------\n")
     print(county_highest_turnout_summary)
     # Write county highest turnout summart to text file
     txt_file.write(county_highest_turnout_summary)
@@ -110,7 +99,7 @@ with open(file_to_save, "w") as txt_file:
         votes = candidate_votes[candidate]
         vote_percentage = float(votes) / float(total_votes) * 100
         candidate_results = (
-            f"{candidate}: {vote_percentage:.1f}% ({votes:,})")
+            f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
 
         # Print each candidate's voter count and percentage to the terminal.
         print(candidate_results)
